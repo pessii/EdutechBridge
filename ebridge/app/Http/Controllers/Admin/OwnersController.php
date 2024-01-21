369ae8e0013654ad4f4d79b0fff457f14b1af84b
kpt_suzuki <868802338618>
2024-01-22 00:23:45 +0900

    ページネーション

--- ebridge/app/Http/Controllers/Admin/OwnersController.php
+++ ebridge/app/Http/Controllers/Admin/OwnersController.php
@@ -29,7 +29,8 @@ class OwnersController extends Controller
      */
     public function index()
     {
-        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')
+            ->paginate(10);
 
         return view('admin.owners.index', 
             compact(
