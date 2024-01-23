4d9a155522cc4ec28f26f5f15d922656ae480735
kpt_suzuki <868802338618>
2024-01-23 20:32:45 +0900

    リサイズ処理

--- ebridge/app/Http/Controllers/Owner/ShopController.php
+++ ebridge/app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
+use InterventionImage;
+
 class ShopController extends Controller
 {
     public function __construct()
@@ -75,9 +77,21 @@ class ShopController extends Controller
     {
         //一時保存 
         $imageFile = $request->image;
-        if(!is_null($imageFile) && $imageFile->isValid() ){ 
+        if(!is_null($imageFile) && $imageFile->isValid() ){
+            // (リサイズなし) 
             // ファイル名の一意のIDを自動的に生成して保存
-            Storage::putFile('public/shops', $imageFile); 
+            // Storage::putFile('public/shops', $imageFile); 
+
+            // (リサイズあり)
+            // ランダムなファイル名を作成
+            $fileName = uniqid(rand().'_'); 
+            // 拡張子を取得
+            $extension = $imageFile->extension(); 
+            $fileNameToStore = $fileName. '.' . $extension; 
+            // リサイズ処理
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
         }
 
         return redirect()->route('owner.shops.index');
