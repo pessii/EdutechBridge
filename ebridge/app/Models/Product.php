1d77593a36b418160700381f15459fea4662178c
kpt_suzuki <868802338618>
2024-01-24 21:54:10 +0900

    model記載

--- ebridge/app/Models/Product.php
+++ ebridge/app/Models/Product.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Shop;
+use App\Models\SecondaryCategory;
+use App\Models\Image;
 
 class Product extends Model
 {
@@ -14,5 +16,15 @@ class Product extends Model
     public function shop()
     {
         return $this->belongsTo(Shop::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(SecondaryCategory::class, 'secondary_category_id');
+    }
+
+    public function imageFirst()
+    {
+        return $this->belongsTo(Image::class, 'image1', 'id');
     }
 }
