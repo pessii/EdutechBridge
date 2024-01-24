42189f329c32fded30287b2ec23066ec35610c55
kpt_suzuki <868802338618>
2024-01-25 01:07:01 +0900

    プロダクトの作成

--- ebridge/database/migrations/2024_01_24_213300_create_products_table.php
+++ ebridge/database/migrations/2024_01_24_213300_create_products_table.php
@@ -15,9 +15,19 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')->nullable();
             $table->foreignId('shop_id')->constrained();
             $table->foreignId('secondary_category_id')->constrained();
             $table->foreignId('image1')->nullable()->constrained('images');
+            $table->foreignId('image2')->nullable()->constrained('images');
+            $table->foreignId('image3')->nullable()->constrained('images');
+            $table->foreignId('image4')->nullable()->constrained('images');
+            $table->foreignId('image5')->nullable()->constrained('images');
+            $table->foreignId('image6')->nullable()->constrained('images');
             $table->timestamps();
         });
     }
--- ebridge/resources/views/components/select-image.blade.php
+++ ebridge/resources/views/components/select-image.blade.php
@@ -3,11 +3,13 @@
     if($name === 'image2'){ $modal = 'modal-2'; }
     if($name === 'image3'){ $modal = 'modal-3'; }
     if($name === 'image4'){ $modal = 'modal-4'; }
+    if($name === 'image5'){ $modal = 'modal-5'; }
+    if($name === 'image6'){ $modal = 'modal-6'; }
 
-    if($name === 'image4'){ $modal = 'modal-5'; }
+    if($name === 'image7'){ $modal = 'modal-5'; }
 @endphp
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
         <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
 
             <header class="modal__header">
@@ -41,9 +43,9 @@
     </div>
 </div>
 
-<div class="flex justify-around items-center mb-4"> 
-    <a data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
-    <div class="w-1/4"> 
+<div class="flex items-center mb-4">
+    <a class="py-2 px-4 bg-gray-200" data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
+    <div class="w-1/4 ml-auto"> 
         <img id="{{ $name }}_thumbnail" src=""> 
     </div> 
 </div> 
