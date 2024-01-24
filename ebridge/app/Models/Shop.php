389c8ec9706726d42e446ef22c90e3bf96e97771
kpt_suzuki <868802338618>
2024-01-24 21:46:36 +0900

    プロダクトのDB作成

--- ebridge/app/Models/Shop.php
+++ ebridge/app/Models/Shop.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Owner;
+use App\Models\Product;
 
 class Shop extends Model
 {
@@ -27,5 +28,10 @@ class Shop extends Model
     public function owner()
     {
         return $this->belongsTo(Owner::class);
+    }
+
+    public function product()
+    {
+        return $this->hasMany(Product::class);
     }
 }
