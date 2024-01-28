b17a117366e1fc56c860cc710fbfd547cc63f2ad
kpt_suzuki <868802338618>
2024-01-28 20:13:09 +0900

    在庫詳細

--- ebridge/app/Http/Controllers/User/ItemController.php
+++ ebridge/app/Http/Controllers/User/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\Stock;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
@@ -59,9 +60,17 @@ class ItemController extends Controller
     {
         $product = Product::findOrFail($id);
 
+        $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+        // 数量が10より多かったら10にする
+        if($quantity > 10){ 
+            $quantity = 10; 
+        }
+
         return view('user.show',
             compact(
-                'product'
+                'product',
+                'quantity'
             )
         );
     }
