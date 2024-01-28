a5f22b65e5298bb11bf9658c7bd05f234f6fd694
kpt_suzuki <868802338618>
2024-01-28 22:40:32 +0900

    ストライプ設定

--- ebridge/app/Http/Controllers/User/CartController.php
+++ ebridge/app/Http/Controllers/User/CartController.php
@@ -58,4 +58,41 @@ class CartController extends Controller
         return redirect()->route('user.cart.index'); 
     }
 
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+        $lineItems = []; 
+        foreach($user->products as $product){ 
+            $lineItem = [ 
+                'price_data.product_data.name' => $product->name, 
+                'price_data.product_data.description' => $product->description, 
+                'price_data.unit_amoun' => $product->price, 
+                'price_data.currency' => 'jpy', 
+                'quantity' => $product->pivot->quantity, 
+            ]; 
+            array_push($lineItems, $lineItem); 
+        }
+        
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout',
+                compact(
+                    'session',
+                    'publicKey'
+                )
+            );
+    }
+
 }
