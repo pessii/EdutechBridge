7bc0b32d95d189427cc2c34efa20d7dbfdfb41bb
kpt_suzuki <868802338618>
2024-01-28 12:49:55 +0900

    コンスタントの追加処理

--- ebridge/app/Http/Controllers/Owner/ProductController.php
+++ ebridge/app/Http/Controllers/Owner/ProductController.php
@@ -212,10 +212,12 @@ class ProductController extends Controller
                     $product->is_selling = $request->is_selling;
                     $product->save();
 
-                    if($request->type === '1') {
+                    // 追加処理
+                    if($request->type === \Constant::PRODUCT_LIST['add']) {
                         $newQuantity = $request->quantity;
                     } 
-                    if($request->type === '2') {
+                    // 削減処理
+                    if($request->type === \Constant::PRODUCT_LIST['reduce']) {
                         $newQuantity = $request->quantity * -1;
                     }
 
