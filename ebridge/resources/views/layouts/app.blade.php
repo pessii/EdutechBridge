b7a545ab16aba8ab5976f7a2f613a24797004990
kpt_suzuki <868802338618>
2024-02-01 00:13:24 +0900

    英語から日本語に変更

--- ebridge/resources/views/layouts/app.blade.php
+++ ebridge/resources/views/layouts/app.blade.php
@@ -16,9 +16,8 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @if(auth('admin')->user())
-                @include('layouts.admin-navigation')
-            @elseif(auth('owners')->user())
+            <!-- adminは現状使わない -->
+            @if(auth('owners')->user())
                 @include('layouts.owner-navigation')
             @elseif(auth('users'))
                 @include('layouts.user-navigation')
